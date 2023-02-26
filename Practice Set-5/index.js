b020975c430c66b9af7de7dc0faf726188d6132d
Mayuri Mahto <mahtomayuri@gmail.com>
2023-02-26 16:51:46 +0530

    set-5

---
+++ Practice Set-5/index.js
@@ -0,0 +1,10 @@
+//Q-1
+//Create an array of numbers and take input from the user to add numbers to this array
+//Q-2
+//Keep adding numbers to the array in q-1 until 0 is added to the array
+//Q-3
+//Filter for numbers divisible by 10 from a given array
+//Q-4
+//Create an array of square of given numbers
+//Q-5
+//Use reduce to calculate factorial of a given number from an array of 1st n natural number( n being the number whose factorial needs to be calculated)
